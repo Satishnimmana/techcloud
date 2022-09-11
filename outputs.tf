fe54dadd6d0829397d9ec8d56024b94e26f38848
Satishnimmana <satishnimmana31@gamil.com>
2022-09-11 20:48:17 +0530

    crertaeservers

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,15 @@
+# EC2 Instance Public IP
+output "instance_publicip" {
+  description = "EC2 Instance Public IP"
+  value = aws_instance.myec2vm.public_ip
+}
+
+# EC2 Instance Public DNS
+output "instance_publicdns" {
+  description = "EC2 Instance Public DNS"
+  value = aws_instance.myec2vm.public_dns
+}
 output "instance_state"{
  description = "ec2 Instance status"
- value = "instance_state"
+ value = aws_instance.myec2vm.instance_state
 } 
