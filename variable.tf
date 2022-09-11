ac240490ec65ad2cea839504b9bb5387b364ffde
Satishnimmana <satishnimmana31@gamil.com>
2022-09-11 20:11:39 +0530

    hello

--- variable.tf
+++ variable.tf
@@ -1,3 +1,9 @@
+variable "aws_region" {
+  description = "Region in which AWS Resources to be created"
+  type = string
+  default = "us-east-1"
+}
+
 variable "instance_type" {
   description = "EC2 Instnace Type"
   type = string
